99e0b0bdcaae1f87500fe0cfca09a43411ef7f53
Christian Müller <christian.s.mueller@accenture.com>
2013-02-21 15:30:53 +0100

    initial commit

---
+++ Mineral.js
@@ -0,0 +1,60 @@
+"use strict";
+
+function _flattenList(memo, list) {
+	if (!list.head) return memo;
+	memo.push(list.head);
+	return flattenList(memo, list.tail);
+}
+
+function _createAtom(x) {
+	return {
+		"atom": true,
+		"value": x
+	};
+}
+
+var quote = function(x) {
+	return x;
+};
+
+var atom = function(x) {
+	return _createAtom(x.atom || (x.list && !x.head));
+};
+
+var eq = function(args) {
+	var a = args[0], b = args[1];
+	return _createAtom(	(a.atom && b.atom && a.value == b.value) || 
+						(a.list && b.list && !a.head && !b.head));
+}
+
+var head = function(list) {
+	return list.head;
+}
+
+var tail = function(list) {
+	return list.tail;
+}
+
+var cons = function(args) {
+	var element = args[0], list = args[1];
+	return { "head": element, "tail": list };
+}
+
+var cond = function(pairs) {
+	for (var pair in pairs)
+		if(eval(pair.1).value == true)
+			return eval(pair.2);
+}
+
+function eval(x) {
+	if (x.atom)
+		return x.value;
+	else {
+		var f = x.head;
+		var args = _flattenList([], x.tail)
+		if(f != cond && f != quote)
+			for(var i = 0; i < args.length; i++)
+				args[i] = eval(args[i]);
+		return f(args);
+	}
+}
