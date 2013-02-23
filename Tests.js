39f821dad2ecdbdc7c871c8eaec907a2fa5a8e13
Christian Mueller <chmllr@me.com>
2013-02-23 14:39:29 +0100

    tests moved to a separate file

---
+++ Tests.js
@@ -0,0 +1,30 @@
+function runTests() {
+	var counter = 0;
+	var assertEqual = function(input, output) {
+		if(output == stringify(evaluate(parse(input))))
+			console.log("Test " + (counter++) + " successful: execution of '"
+						+ input + "' produces '" + output + "'");
+		else console.error("Test failed: execution of '"
+							+ input + "' doesn't produce '" + output + "'");
+	}
+
+	assertEqual("(quote a)", "a");
+	assertEqual("(quote (a b c))", "(a b c)");
+	assertEqual("(atom (quote a))", true);
+	assertEqual("(atom (quote (a b c)))", false);
+	assertEqual("(atom (quote NIL))", true);
+	assertEqual("(atom (atom (quote a)))", true);
+	assertEqual("(atom (quote (atom (quote a))))", false);
+	assertEqual("(eq (quote a) (quote a))", true);
+	assertEqual("(eq (quote a) (quote b))", false);
+	assertEqual("(eq (quote NIL) (quote NIL))", true);
+	assertEqual("(head (quote (a b c)))", "a");
+	assertEqual("(tail (quote (a b c)))", "(b c)");
+	assertEqual("(cons (quote a) (quote (b c)))", "(a b c)");
+	assertEqual("(cons (quote a) (cons (quote b) (cons (quote c) (quote NIL))))", "(a b c)");
+	assertEqual("(cons (quote a) NIL)", "(a)");
+	assertEqual("(cons (quote a) (quote NIL))", "(a)");
+	assertEqual("(branch (eq (quote a) (quote b)) (quote first) (quote second))", "second");
+	assertEqual("(branch (eq NIL NIL) (quote first) (quote second))", "first");
+
+}
