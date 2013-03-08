2e9069de6fc770901191edca1c695b5e603c31c7
Christian Mueller <chmllr@me.com>
2013-03-08 10:32:57 +0100

    Tests renumbered

--- Tests.js
+++ Tests.js
@@ -30,15 +30,15 @@ function runTests() {
 	assertEqual(20, "((lambda (x y) (cons x (tail y))) 'z '(a b c))", "(z b c)");
 	assertEqual(21, "((lambda (f) (f '(b c))) (lambda (x) (cons 'a x)))", "(a b c)");
 	assertEqual(22, "((def subst (lambda (x y z) (if (atom z) (if (eq z y) x z) (cons (subst x y (head z)) (subst x y (tail z)))))) 'm 'b '(a b (a b c) d))", "(a m (a m c) d)");
-	assertEqual(29, "`()", "()");
-	assertEqual(30, "`~()", "()");
-	assertEqual(31, "`(cons 'a ())", "(cons (quote a) ())");
-	assertEqual(32, "'(cons 'a ())", "(cons (quote a) ())");
-	assertEqual(33, "''(cons 'a ())", "(quote (cons (quote a) ()))");
-	assertEqual(34, "`~'a", "a");
-	assertEqual(35, "`(cons ~(if true 'first 'second) (cons ~(eq 'a 'b) ()))", "(cons first (cons false ()))");
-	assertEqual(36, "((lambda (x y z) (cons y (cons z ()))) (def f (lambda (x) (eq x 'a))) (f 'a) (f 'b))", "(true false)");
-	assertEqual(37, "((macro (x) `(cons ~x '(Z))) 'U)", "(U Z)");
-	assertEqual(38, "((lambda (x y) y) (defmacro mycons (a b) `(cons ~a ~b)) (mycons 'T '(S)))", "(T S)");
-	assertEqual(39, "((lambda (x y) y) (defn mycons (a b) (cons a b)) (mycons 'T '(S)))", "(T S)");
+	assertEqual(23, "`()", "()");
+	assertEqual(24, "`~()", "()");
+	assertEqual(25, "`(cons 'a ())", "(cons (quote a) ())");
+	assertEqual(26, "'(cons 'a ())", "(cons (quote a) ())");
+	assertEqual(27, "''(cons 'a ())", "(quote (cons (quote a) ()))");
+	assertEqual(28, "`~'a", "a");
+	assertEqual(29, "`(cons ~(if true 'first 'second) (cons ~(eq 'a 'b) ()))", "(cons first (cons false ()))");
+	assertEqual(30, "((lambda (x y z) (cons y (cons z ()))) (def f (lambda (x) (eq x 'a))) (f 'a) (f 'b))", "(true false)");
+	assertEqual(31, "((macro (x) `(cons ~x '(Z))) 'U)", "(U Z)");
+	assertEqual(32, "((lambda (x y) y) (defmacro mycons (a b) `(cons ~a ~b)) (mycons 'T '(S)))", "(T S)");
+	assertEqual(33, "((lambda (x y) y) (defn mycons (a b) (cons a b)) (mycons 'T '(S)))", "(T S)");
 }
