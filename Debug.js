6f62e5c7985b6971da2da21e74b858d222ac0c81
Christian Mueller <chmllr@me.com>
2013-03-29 17:33:04 +0100

    patching improved

--- Debug.js
+++ Debug.js
@@ -1,24 +1,22 @@
-function patchMineral (pre, post) {
+function patchFn(f, key, pre, post){
+    return function () {
+        pre(key, arguments);
+        var result = f.apply(f, arguments);
+        post(key, result);
+        return result;
+    }
+}
 
+function patchMineral (pre, post) {
     var id = function(){};
     pre = pre ? pre : id;
     post = post ? post : id;
-
     for(var key in mineral)
     {
-        var f = mineral[key];
-        mineral[key] = (function(key, f) {
-            return function () {
-                pre(key, arguments);
-                var result = f.apply(f, arguments);
-                post(key, result);
-                return result;
-            }
-        })(key, f);
+        mineral[key] = patchFn(mineral[key], key, pre, post);
         // we only want to patch primitives
         if(key == "externalcall") return;
     }
-    
 }
 
 var printFnName = function () {
